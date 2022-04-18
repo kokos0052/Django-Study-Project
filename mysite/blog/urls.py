75997304e4b53f570f01c8b76c3b5620ad96276e
Isa <izbagov0133@gmail.com>
2022-04-18 22:41:44 +0300

    Brocked App)

---
+++ mysite/blog/urls.py
@@ -0,0 +1,12 @@
+import imp
+from djang.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:mounth>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+]
+
